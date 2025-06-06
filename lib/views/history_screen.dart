39a4ea66459ad4c63b4ebbd33773f670e86b19e8
siljavind <silja.vj@gmail.com>
2025-06-06 10:02:14 +0200

    navigation bar and basic scaffolding done

---
+++ lib/views/history_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class HistoryScreen extends StatelessWidget {
+  const HistoryScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('History')),
+      body: const Center(
+        child: Text(
+          'History placeholder',
+          style: TextStyle(fontSize: 18),
+        ),
+      ),
+    );
+  }
+}
